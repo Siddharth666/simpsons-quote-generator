520e33296ca01bcf4fb41536a850c8c6ceb91f88
siddharth.singha <siddharth.singha@datamoulds.com>
2020-03-07 23:31:00 +0530

    First simpsons commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-quotes-app';
+
+  constructor() { }
+   simp_character: string;
+   simp_quotes: string;
+   simp_image: string;
+
+  ngOnInit() {
+    //alert("Hello Sidd")
+    //this.someFunction();
+  }
+
+  someFunction() {
+     axios.get("https://thesimpsonsquoteapi.glitch.me/quotes")
+    .then((response)=> {
+      this.simp_character = response.data[0].character;
+      this.simp_quotes = response.data[0].quote;
+      this.simp_image = response.data[0].image;
+    })
+    .catch((error)=> {
+      console.log(error)
+    })
+  }
+//https://thesimpsonsquoteapi.glitch.me/
+
+clickEvent() {
+  this.someFunction();
+}
+
 }
---
+++ src/app/quotes-app/quotes-app.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuotesAppComponent } from './quotes-app.component';
+
+describe('QuotesAppComponent', () => {
+  let component: QuotesAppComponent;
+  let fixture: ComponentFixture<QuotesAppComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QuotesAppComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuotesAppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
